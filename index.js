c297807874efd350464f6568f30a59de5f53bdd2
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-09 09:01:06 -0500

    Returning article titles and urls

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ const url = 'https://www.theguardian.com/uk'
 axios(url)
     .then(response => {
         const html = response.data
-        console.log(html)
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.fc-item__title', html).each(function() {
+           const title =  $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        })
+        console.log(articles)
+    })
+    .catch(error => {
+        console.log(error)
     })
 
 
