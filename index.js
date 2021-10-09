34688e19ee4aa5f0280f48fae682bdc741bcecb0
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-08 19:23:33 -0500

    Imported all libraries

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const axios = require('axios')
+const cheerio = require("cheerio")
 const express = require('express')
 const app = express()
 const PORT = 3000
+
+
+
+
 
 
 app.listen(PORT, () => {
