65f2bbd5aac40b318ef66a0613a3b79c19fa6eed
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-12 17:00:46 -0500

    Setup with new link to provide character movesets for Nickolodean

--- index.js
+++ index.js
@@ -4,29 +4,36 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-const url = 'https://www.theguardian.com/uk'
+// const url = 'https://www.theguardian.com/uk'
+const url = 'https://nickelodeonallstarbrawl.com/'
+
 
 axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        const articles = []
-        $('.fc-item__title', html).each(function() {
-           const title =  $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
-            })
-        })
-        console.log(articles)
+    //     const articles = []
+    //     $('.fc-item__title', html).each(function() {
+    //        const title =  $(this).text()
+    //         const url = $(this).find('a').attr('href')
+    //         articles.push({
+    //             title,
+    //             url
+    //         })
+    //     })
+    //     console.log(articles)
+    // })
+    // .catch(error => {
+    //     console.log(error)
+    // })
+
+    const links = []
+    $('.moves', html).each(function() {
+        const image =  $(this).find('a').attr('href')
+        links.push({image})
     })
-    .catch(error => {
-        console.log(error)
+console.log(links)
     })
-
-
-
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
